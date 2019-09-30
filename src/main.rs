f1c909a6d3917e5059be504c8efe260789da0d7a
David Oberacker <david.oberacker@microchip.com>
2019-09-30 17:33:10 +0200

    Added minilzo to dependencies.

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,15 @@ fn main() {
     let string = protobuf::json::print_to_string(&save_game);
     println!("{}", string.unwrap());
     println!("Hello, world!");
+
+    let data = b"foobar_foobar";
+
+    let compressed = minilzo::compress(&data[..]);
+    if (compressed.is_ok()) {
+        let compressed_vector = compressed.unwrap();
+        println!("{}", String::from_utf8(compressed_vector.clone()).unwrap());
+
+        let decompressed = minilzo::decompress(&compressed_vector, data.len()).unwrap(); 
+        println!("{}", String::from_utf8(decompressed).unwrap());
+    }
 }
