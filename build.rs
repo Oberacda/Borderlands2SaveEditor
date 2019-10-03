3bbce69912aafe57f4b3577f17e070ad26d2e37f
David Oberacker <david.oberacker@gmail.com>
2019-10-03 10:42:06 +0200

    Adapted dependencies.

--- build.rs
+++ build.rs
@@ -1,46 +1,43 @@
 extern crate protoc_rust;
-use std::fs;
 
 use protoc_rust::Customize;
 
 fn main() {
-	protoc_rust::run(protoc_rust::Args {
-	    out_dir: "src/protos",
-	    input: &[
-                "./protos/BankSlot.proto",
-				"./protos/ChallengeData.proto",
-				"./protos/ChosenVehicleCustomization.proto",
-				"./protos/Color.proto",
-				"./protos/DLCExpansionData.proto",
-				"./protos/GoldenKeys.proto",
-				"./protos/GUID.proto",
-				"./protos/InventorySlotData.proto",
-				"./protos/ItemData.proto",
-				"./protos/ItemMemento.proto",
-				"./protos/LockoutData.proto",
-				"./protos/MissionData.proto",
-				"./protos/MissionPlaythroughData.proto",
-				"./protos/MissionStatus.proto",
-				"./protos/OneOffLevelChallengeData.proto",
-				"./protos/PackedItemDataOptional.proto",
-				"./protos/PackedItemData.proto",
-				"./protos/PackedWeaponDataOptional.proto",
-				"./protos/PackedWeaponData.proto",
-				"./protos/PendingMissionRewards.proto",
-				"./protos/PlayerMark.proto",
-				"./protos/QuickWeaponSlot.proto",
-				"./protos/RegionGameStageData.proto",
-				"./protos/ResourceData.proto",
-				"./protos/SkillData.proto",
-				"./protos/UIPreferencesData.proto",
-				"./protos/WeaponData.proto",
-				"./protos/WeaponMemento.proto",
-				"./protos/WillowTwoPlayerSaveGame.proto",
-                "./protos/WorldDiscoveryData.proto"
-            ],
-	    includes: &["protos"],
-	    customize: Customize {
-	      ..Default::default()
-	    },
-	}).expect("protoc");
+	protoc_rust::Args::new()
+		.out_dir("src/protos")
+		.inputs(&[
+			"./protos/BankSlot.proto",
+			"./protos/ChallengeData.proto",
+			"./protos/ChosenVehicleCustomization.proto",
+			"./protos/Color.proto",
+			"./protos/DLCExpansionData.proto",
+			"./protos/GoldenKeys.proto",
+			"./protos/GUID.proto",
+			"./protos/InventorySlotData.proto",
+			"./protos/ItemData.proto",
+			"./protos/ItemMemento.proto",
+			"./protos/LockoutData.proto",
+			"./protos/MissionData.proto",
+			"./protos/MissionPlaythroughData.proto",
+			"./protos/MissionStatus.proto",
+			"./protos/OneOffLevelChallengeData.proto",
+			"./protos/PackedItemDataOptional.proto",
+			"./protos/PackedItemData.proto",
+			"./protos/PackedWeaponDataOptional.proto",
+			"./protos/PackedWeaponData.proto",
+			"./protos/PendingMissionRewards.proto",
+			"./protos/PlayerMark.proto",
+			"./protos/QuickWeaponSlot.proto",
+			"./protos/RegionGameStageData.proto",
+			"./protos/ResourceData.proto",
+			"./protos/SkillData.proto",
+			"./protos/UIPreferencesData.proto",
+			"./protos/WeaponData.proto",
+			"./protos/WeaponMemento.proto",
+			"./protos/WillowTwoPlayerSaveGame.proto",
+			"./protos/WorldDiscoveryData.proto"
+		])
+	.include("protos")
+		.run()
+		.expect("protoc");
 }
